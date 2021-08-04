93ddd8ace4e4ecb622932eda75377bcffc6dab40
Joshua Jameson-Wallis <jjw@Joshuas-MacBook-Air.local>
2021-08-04 17:06:18 +0100

    Dark mode added - switch toggle needs reversing

---
+++ script.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const themeSwitch = document.querySelector('#dark-mode')
+const root = document.querySelector(':root')
+const whiteTxt = document.querySelectorAll('.white-txt')
+const blueTxt = document.querySelectorAll('.blue-txt')
+const gridItems = document.querySelectorAll('.grid-item')
+const instagramGrid = document.querySelector('.hero__grid-item-inst')
+
+themeSwitch.addEventListener('change', e => {
+  if (e.target.matches('#dark-mode')) {
+      root.classList.toggle('html-dark')
+      for (let i = 0; i < whiteTxt.length; i++) whiteTxt[i].classList.toggle('white')
+      for (let i = 0; i < blueTxt.length; i++) blueTxt[i].classList.toggle('desat-blue')
+      for (let i = 0; i < gridItems.length; i++) gridItems[i].classList.toggle('grid-item-dark')
+      instagramGrid.classList.toggle('hero__grid-item-inst-dark')
+  }
+})
